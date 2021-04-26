cbbe20f189ba6113dcec831433148f2daecbbdc4
Nikolay Ananiev <nananiev@positrace.com>
2021-04-26 11:32:30 +0300

    credentials

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 var instanse *ServiceInstanse
 
 func main() {
-	serviceConfig := configuration.ConstructCredentialsJSONProvider(types.NewFile("/config/initializer/credentials.example.json"))
+	serviceConfig := configuration.ConstructCredentialsJSONProvider(types.NewFile("/config/initializer/credentials.json"))
 	credentials, err := serviceConfig.ProvideCredentials()
 	if err != nil {
 		logger.Logger().WriteToLog(logger.Fatal, "[Main] Error while read credentials. Error: ", err.Error())
